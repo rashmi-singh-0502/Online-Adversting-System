3125a3368449145f32025b38470219a7325043f6
patilmayuri3007 <46353596+patilmayuri3007@users.noreply.github.com>
2020-12-08 21:05:14 +0530

    Delete UserDAO.java

--- OnlineAdvertisementSystem/src/main/java/com/jpa/advertiseapp/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.jpa.advertiseapp.dao;
-
-import com.jpa.advertiseapp.entity.UserEntity;
-import com.jpa.advertiseapp.exceptions.UserNotFoundException;
-public interface UserDAO
-{
-	UserEntity findByName(String name) throws UserNotFoundException;
-	
-}
