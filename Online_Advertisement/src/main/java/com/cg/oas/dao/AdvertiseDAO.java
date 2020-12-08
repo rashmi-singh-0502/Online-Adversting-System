165abb2b1aa720970b74768202519ddac788a48f
Rashmi Singh <38667439+rashmi-singh-0502@users.noreply.github.com>
2020-12-08 18:37:47 +0530

    Delete AdvertiseDAO.java

--- Online_Advertisement/src/main/java/com/cg/oas/dao/AdvertiseDAO.java
+++
@@ -1,8 +0,0 @@
-package com.cg.oas.dao;
-
-import com.cg.oas.entity.AdvertiseEntity;
-import com.cg.oas.exceptions.AdvertiseNotFoundException;
-
-public interface AdvertiseDAO {
-	AdvertiseEntity findAdvertiseById(int advertiseId) throws AdvertiseNotFoundException;
-}
