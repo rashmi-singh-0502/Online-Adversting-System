f9fc4d387bf83fe3463c351ba414b9afe9da579e
patilmayuri3007 <46353596+patilmayuri3007@users.noreply.github.com>
2020-12-09 18:20:38 +0530

    Update AdServiceImpl.java

--- OnlineAdvertisingSystem/src/main/java/com/cg/oas/service/AdServiceImpl.java
+++ OnlineAdvertisingSystem/src/main/java/com/cg/oas/service/AdServiceImpl.java
@@ -5,7 +5,9 @@ import org.apache.logging.log4j.Logger;
 import com.cg.oas.dao.AdDAO;
 import com.cg.oas.dao.AdDAOImpl;
 import com.cg.oas.dto.Ad;
-import com.cg.oas.entity.AdEntity;
+import com.cg.oas.dto.Advertise;
+//import com.cg.oas.entity.AdEntity;
+import com.cg.oas.entity.AdvertiseEntity;
 import com.cg.oas.exceptions.InvalidDataFormatException;
 import com.cg.oas.utils.AdAppUtils;
 public class AdServiceImpl implements AdService{
@@ -13,13 +15,12 @@ public class AdServiceImpl implements AdService{
 		private static Logger logger = LogManager.getLogger(AdServiceImpl.class.getName());
 		AdDAO adDAO=new AdDAOImpl();
 		
-		public Ad addData(AdEntity adEntity) throws InvalidDataFormatException
+		public Advertise addData(AdvertiseEntity adEntity) throws InvalidDataFormatException
 		{
 				
-				AdEntity adEntity1=adDAO.postNewAdvertise(adEntity);
+				AdvertiseEntity adEntity1=adDAO.postNewAdvertise(adEntity);
 				return AdAppUtils.convertUserEntityToUser(adEntity1);
 			
 		}
-
-	}
+}
 
