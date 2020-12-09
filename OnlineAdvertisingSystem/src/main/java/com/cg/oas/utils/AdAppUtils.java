271efadbfbcc1547a08fd61e2ff086243e007de7
patilmayuri3007 <46353596+patilmayuri3007@users.noreply.github.com>
2020-12-09 18:21:49 +0530

    Update AdAppUtils.java

--- OnlineAdvertisingSystem/src/main/java/com/cg/oas/utils/AdAppUtils.java
+++ OnlineAdvertisingSystem/src/main/java/com/cg/oas/utils/AdAppUtils.java
@@ -1,15 +1,16 @@
 package com.cg.oas.utils;
 import java.util.List;
-import com.cg.oas.dto.Ad;
-import com.cg.oas.entity.AdEntity;
+import com.cg.oas.dto.Advertise;
+import com.cg.oas.entity.AdvertiseEntity;
 public class AdAppUtils 
 {
 
-		public static Ad convertUserEntityToUser(AdEntity adEntity) {
-			return new Ad(adEntity.getAdvertiseTitle(),adEntity.getAdvertiseCategory(),adEntity.getPrice(),adEntity.getDescription());
+		public static Advertise convertUserEntityToUser(AdvertiseEntity adEntity) {
+			return new Advertise(adEntity.getAd_id(),adEntity.getTitle(),adEntity.getCategory(),adEntity.getDescription(),adEntity.getPrice(),adEntity.getStatus());
 		} 
 		
-		public static AdEntity coverUserToUSerEntity(Ad ad) {
-			return new AdEntity(ad.getAdvertiseTitle(),ad.getCategory(),ad.getPrice(),ad.getDescription());
+		public static AdvertiseEntity coverUserToUSerEntity(Advertise ad) {
+			return new AdvertiseEntity(ad.getAd_id(),ad.getTitle(),ad.getCategory(),ad.getDescription(),ad.getPrice(),ad.getStatus());
 		}
 }
+
