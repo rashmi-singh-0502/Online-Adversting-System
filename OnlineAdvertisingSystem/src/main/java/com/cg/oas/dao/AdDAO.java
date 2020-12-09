a938b6a4d5f221901500068e1163b4d4a6acea31
patilmayuri3007 <46353596+patilmayuri3007@users.noreply.github.com>
2020-12-09 18:07:02 +0530

    Update AdDAO.java

--- OnlineAdvertisingSystem/src/main/java/com/cg/oas/dao/AdDAO.java
+++ OnlineAdvertisingSystem/src/main/java/com/cg/oas/dao/AdDAO.java
@@ -1,9 +1,10 @@
 package com.cg.oas.dao;
 
-import com.cg.oas.entity.AdEntity;
+import com.cg.oas.entity.AdvertiseEntity;
 import com.cg.oas.exceptions.InvalidDataFormatException;
 
 public interface AdDAO 
 {
-	AdEntity postNewAdvertise(AdEntity adEntity) throws InvalidDataFormatException;
+	AdvertiseEntity postNewAdvertise(AdvertiseEntity adEntity) throws InvalidDataFormatException;
 }
+
