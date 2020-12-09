e48303771bfaa80c3374a932ef384b121407e638
MAHIMA-06 <52665050+MAHIMA-06@users.noreply.github.com>
2020-12-09 19:18:36 +0530

    Delete AdDao.java

--- OnlineAdvertisingSystem/src/main/java/com/cg/oas/dao/AdDao.java
+++
@@ -1,9 +0,0 @@
-package com.cg.oas.dao;
-
-import com.cg.oas.entity.AdEntity;
-import com.cg.oas.exceptions.AdNotFoundException;
-
-
-public interface AdDao {
-	AdEntity findAdvertiseById(int advertiseId) throws AdNotFoundException;
-}
