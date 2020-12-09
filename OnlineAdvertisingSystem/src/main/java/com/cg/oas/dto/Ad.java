0749b0d281d23a6bb321d35c7502c1227031a6f5
vishakhadhote7 <41178429+vishakhadhote7@users.noreply.github.com>
2020-12-09 09:47:25 +0530

    Add files via upload

--- OnlineAdvertisingSystem/src/main/java/com/cg/oas/dto/Ad.java
+++ OnlineAdvertisingSystem/src/main/java/com/cg/oas/dto/Ad.java
@@ -1,60 +1,71 @@
 package com.cg.oas.dto;
-public class Ad 
+
+public class Ad
 {
-	private String advertiseTitle;
+	private int ad_id;
+	private String title;
 	private String category;
-	private int price;
 	private String description;
-	
-	public Ad(String advertiseTitle, String category, int price, String description) {
+	private double price;
+	private String status;
+	public Ad(int ad_id, String title, String category, String description, double price, String status) {
 		super();
-		this.advertiseTitle = advertiseTitle;
+		this.ad_id = ad_id;
+		this.title = title;
 		this.category = category;
-		this.price = price;
 		this.description = description;
+		this.price = price;
+		this.status = status;
 	}
-
-	public Ad() {
+	public Ad(String title, String category, String description, double price, String status) {
 		super();
+		this.title = title;
+		this.category = category;
+		this.description = description;
+		this.price = price;
+		this.status = status;
 	}
-
-	public String getAdvertiseTitle() {
-		return advertiseTitle;
+	public Ad() {
 	}
-
-	public void setAdvertiseTitle(String advertiseTitle) {
-		this.advertiseTitle = advertiseTitle;
+	public int getAd_id() {
+		return ad_id;
 	}
-
+	public void setAd_id(int ad_id) {
+		this.ad_id = ad_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 	public String getCategory() {
 		return category;
 	}
-
 	public void setCategory(String category) {
 		this.category = category;
 	}
-
-	public int getPrice() {
-		return price;
-	}
-
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
 	public String getDescription() {
 		return description;
 	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 	@Override
 	public String toString() {
-		return "AdvertiseEntity [advertiseTitle=" + advertiseTitle + ", category=" + category + ", price=" + price
-				+ ", description=" + description + "]";
+		return "Advertise [ad_id=" + ad_id + ", title=" + title + ", category=" + category + ", description="
+				+ description + ", price=" + price + ", status=" + status + "]";
 	}
-	
-	
 }
