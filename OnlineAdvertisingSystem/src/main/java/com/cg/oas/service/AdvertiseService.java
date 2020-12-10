1fb692d4d487f70bcab6f9445a12d814181ed0b5
vishakhadhote7 <41178429+vishakhadhote7@users.noreply.github.com>
2020-12-10 12:34:06 +0530

    Delete AdvertiseService.java

--- OnlineAdvertisingSystem/src/main/java/com/cg/oas/service/AdvertiseService.java
+++
@@ -1,8 +0,0 @@
-package com.cg.oas.service;
-
-import com.cg.oas.dto.Advertise;
-import com.cg.oas.exceptions.AdvertiseNotFound;
-
-public interface AdvertiseService {
-	Advertise findAdvertiseTitle(String  advertisename) throws AdvertiseNotFound;
-}
